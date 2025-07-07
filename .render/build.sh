1111bb884eff0fb4629101d7a7cb2106c4c159ab
Truongjava <thanhtruong16042004@gmail.com>
2025-07-07 13:23:41 +0700

    Add render.yaml to fix playwright install

--- .render/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Cập nhật pip và cài thư viện
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Tải trình duyệt Chromium
-playwright install chromium
-ls -la ~/.cache/ms-playwright
